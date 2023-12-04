a9faa40c20535861f6ca7aa78b3f2d9c98002964
Your Name <you@example.com>
2023-12-04 15:36:58 +0000

    Added the first tag

--- remote_backend.tf
+++ remote_backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "remote" {
-    hostname = "app.terraform.io"
+    hostname     = "app.terraform.io"
     organization = "henry-lab"
     workspaces {
       name = "hashicat-aws"
